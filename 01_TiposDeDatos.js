0e1d56160c9d235d0ffe260a048b765962d46ba0
Jzsimmonds <65059436+Jzsimmonds@users.noreply.github.com>
2024-12-30 11:28:03 -0500

    Add files via upload

---
+++ 01_TiposDeDatos.js
@@ -0,0 +1,53 @@
+/**
+ * TIPOS DE DATOS
+ *
+ * Numero -- Number
+ *
+ */
+console.log(27); // Tipo numero
+// usamos la extension snipet --clg-> enter
+console.log(27.7); // Tipo float
+
+//Tipo Texto --String
+("Jose");
+("Simmonds");
+console.log("Simmonds");
+console.log("Jose");
+
+//Tipo logico --Booleanos o Boolean
+// Son TRUE or FALSE
+console.log(true);
+console.log(false);
+
+//Tipo de dato Indefinido --Undefined
+console.log(undefined);
+
+//Tipo de dato Null
+console.log(null);
+
+/**
+ * Tipos de Datos Esctructurales
+ *
+ * Tipo Objeto
+ * Puede almacenar cualquier tipo de dato
+ * Syntax
+ *   { Propiedades: Key - Value}
+ *  { key: value}
+ */
+
+//Ejemplo --Object
+console.log({
+  nombre: "Jose",
+  edad: 24,
+  fecha_de_nacimiento: null,
+  alto: false,
+});
+// There is 3 objects (nombre, edad, birth date, tall)
+// There is 3 values (jose, 24, null, false)
+
+// Tipo de Dato Lista --Array
+["Panama", "United States", "Canada"];
+
+//Ejemplo
+console.log(["panama", "United States", "Canada"]);
+console.log([{ Nombre: "Jose" }, [1, 2, 3, 4]]);
